690e653e5484ed35950c3ac7d3af8e00a69b4c53
shafiq0225 <shafiqahamed.be@gmail.com>
2024-12-23 22:24:30 +0530

    header filter ui component

--- AirBNB-Frontend-clone/src/app/cmps/header-filter-folder/header-filter/header-filter.component.ts
+++ AirBNB-Frontend-clone/src/app/cmps/header-filter-folder/header-filter/header-filter.component.ts
@@ -1,12 +1,86 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
+
+export interface Order {
+  _id: string,
+  buyer: {
+    _id: string,
+    fullname: string
+  },
+  totalPrice: number,
+  startDate: Date,
+  endDate: Date,
+  guests: Guest,
+  stay: {
+    _id: string,
+    name: string,
+    price: number
+  },
+  host: {
+    _id: string,
+    fullname: string,
+  }
+  status: string
+}
+export interface Guest {
+  adults: number
+  children: number
+  infants: number
+  pets: number
+}
 
 @Component({
   selector: 'app-header-filter',
   templateUrl: './header-filter.component.html',
   styleUrl: './header-filter.component.scss'
 })
+
+
 export class HeaderFilterComponent {
+  @Input() isHeaderFilterActive!: boolean;
+  @Output() toggleHeaderFilter = new EventEmitter<void>()  
   faMagnifyingGlass = faMagnifyingGlass
-  
+  modalNav = '';
+  date: string | null = null;
+  order !: Order
+  onToggleHeaderFilter() {
+    this.toggleHeaderFilter.emit()
+  }
+
+  onClickDate(val: string) {
+    this.modalNav = val
+  }
+
+  getCheckIn() {
+    if (this.date) {
+      const dates = this.date?.split('-')
+      if (dates.length >= 1) {
+        this.order.startDate = new Date(dates[0])
+        return new Date(dates[0])
+      }
+    }
+    return
+  }
+
+  getCheckOut() {
+    if (this.date) {
+      const dates = this.date?.split('-')
+      if (dates.length === 2) {
+        this.order.endDate = new Date(dates[1])
+        return new Date(dates[1])
+      }
+    }
+    return
+  }
+
+  setModalNav(val: string) {
+    this.modalNav = val
+  }
+
+  getGuests() {
+    // let str = this.order.guests.adults + this.order.guests.children > 0 ? (this.order.guests.adults + this.order.guests.children) + ' guests ' : ''
+    // str += this.order.guests.infants > 0 ? ' ,' + this.order.guests.infants + ' infants ' : ''
+    // str += this.order.guests.pets > 0 ? ' ,' + this.order.guests.pets + ' pets ' : ''
+    // return str
+  }
 }
--- AirBNB-Frontend-clone/src/app/cmps/header/header.component.ts
+++ AirBNB-Frontend-clone/src/app/cmps/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { faBars } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
@@ -10,8 +10,13 @@ export class HeaderComponent {
   faBars = faBars;
   isShowHeaderMenuModal: boolean = false
   isOpenLanguageModal: boolean = false
-  
+  @Input() isShowFilter!: boolean
+  isHeaderFilterActive: boolean = false
   onToggleHeaderMenuModal() {
     this.isShowHeaderMenuModal = !this.isShowHeaderMenuModal
   } 
+
+  toggleHeaderFilter() {
+    this.isHeaderFilterActive = !this.isHeaderFilterActive
+  }
 }
